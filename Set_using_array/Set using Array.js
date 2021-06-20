1ad2432c3f3b54e68d67727812d8bdb034e7aa41
Shafayet Hossain <shafayethossain777@gmail.com>
2021-06-20 21:24:31 +0600

    Set using Array

---
+++ Set_using_array/Set using Array.js
@@ -0,0 +1,71 @@
+
+
+class Set{
+    constructor(array){
+       this.ar=[]
+       this.store={}
+       for(let i=0;i<array.length;i++){
+            
+        if(this.ar.indexOf(array[i])==-1){
+            this.store[i]=array[i]
+        }
+        this.ar.push(array[i])
+       }
+        this.ar=[]  
+        
+    }
+   
+
+   size(){
+       let count=0
+       for(let v in this.store){
+           count++
+       }
+       console.log(count)
+   }
+   
+   has(val){
+       for(let v in this.store){
+           if(this.store[v]==val){
+               return true
+           }
+       }
+       return false
+   }
+   add(val){
+       let ischeck=null
+       let counter=0       
+       for(let v in this.store){
+           if(this.store[v]==val){
+               ischeck=true
+           }
+           counter++
+       }
+        console.log(counter)
+       if(!ischeck){
+           this.store[counter]=val
+       }
+   }
+
+   delete(val){
+       let obj={};
+       let counter=0
+       for(let v in this.store){
+           if(this.store[v]!=val){
+               obj[counter]=this.store[v]
+               counter++
+           }
+       }
+       this.store=obj
+   }
+}
+
+const s=new Set([1,2,3,4,3])
+console.log(s)
+s.size()
+console.log(s.has(3))
+s.add(5)
+console.log(s)
+s.delete(3)
+s.delete(1)
+console.log(s)
